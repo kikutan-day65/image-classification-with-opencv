b7f94c6702917c3cf05c298dc90929cda65b6886
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-08-16 15:32:43 +0900

    feat: add function to find contours

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -1,25 +1,44 @@
 import cv2 as cv
 
 
-def show_canny(img_path):
+def detect_edge(img_path):
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
 
     resized_img = cv.resize(img, (400, 600))
 
     blur = cv.GaussianBlur(resized_img, (3, 3), 3)
 
-    edge_detection = cv.Canny(blur, threshold1=100, threshold2=200, L2gradient=True)
+    detected_edge = cv.Canny(blur, threshold1=200, threshold2=250, L2gradient=True)
 
-    cv.imshow('Detected edge', edge_detection)
+    return detected_edge
 
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    # cv.imshow('Detected edge', edetected_edge)
+
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
+
+def find_contour(edge_detected):
+    contours, hierarchy = cv.findContours(edge_detected, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    return contours
+
+    # contour_img = cv.cvtColor(edge_detected, cv.COLOR_GRAY2BGR)
+    # cv.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
+
+    # cv.imshow('found contours', contour_img)
+
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
 
 
 def main():
-    img_path = 'test/test-164.jpg'
+    img_path = 'test/test-128.jpg'
 
-    show_canny(img_path)
+    edge_detected = detect_edge(img_path)
+
+    found_contour = find_contour(edge_detected)
 
 
 if __name__ == "__main__":
