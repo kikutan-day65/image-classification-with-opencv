73c01c2733d3dd30b06ca4966dd3aa679af337c8
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-08-18 13:55:56 +0900

    feat: find contour and hierarchy funcion

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -1,7 +1,5 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 def detect_edge(img_path):
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
@@ -30,12 +28,28 @@ def calc_edge_density(edge_detected):
     return edge_density
 
 
+def find_contour(edge_detected):
+    contours, hierarchy = cv.findContours(edge_detected, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    return contours, hierarchy
+
+    # contour_img = cv.cvtColor(edge_detected, cv.COLOR_GRAY2BGR)
+    # cv.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
+
+    # cv.imshow('found contours', contour_img)
+
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
+
 def main():
     img_path = 'test/test-128.jpg'
 
     edge_detected = detect_edge(img_path)
 
     edge_density = calc_edge_density(edge_detected)
+
+    contour_and_hierarchy = find_contour(edge_detected)
 
 
 if __name__ == "__main__":
