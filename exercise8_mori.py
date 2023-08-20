5ba427be3698aa5146ff03035ec7b2b2e8edacb8
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-08-20 15:08:40 +0900

    feat: change the variable name

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -2,18 +2,16 @@ import cv2 as cv
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.cluster import KMeans
+
 
 
 def main():
-    img_path = []
+    img_paths = []
 
     dir = 'test'
     for filename in os.listdir(dir):
-        path = os.path.join(dir, filename)
-        img_path.append(path)
-
+        img_path = os.path.join(dir, filename)
+        img_paths.append(img_path)
 
 
 if __name__ == "__main__":
