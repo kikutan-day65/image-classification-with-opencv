437d178f49297a988578de1891f0f9b3b212be0b
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-08-21 02:02:53 +0900

    feat: calculate average sturation of image function and calssify images and others based on it

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+
 
 def detect_edges(img_path):
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
@@ -19,21 +19,36 @@ def find_contours(edge_detected):
     return contours
 
 
+def calc_average_saturation(img_path):
+    image = cv.imread(img_path)
+
+    hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+    saturation_channel = hsv_image[:, :, 1]
+
+    average_saturation = np.mean(saturation_channel)
+
+    return average_saturation
+
+
+def classify_image_and_others(img_path, filename, saturation):
+    img = cv.imread(img_path)
+
+    if saturation > 5:
+        cv.imwrite('result/image/' + filename, img)
+    else:
+        pass
+
+
 def main():
-    # img_paths = []
+    dir = 'test'
+    for filename in os.listdir(dir):
+        img_path = os.path.join(dir, filename)
+            
+        average_saturation = calc_average_saturation(img_path)
 
-    # dir = 'test'
-    # for filename in os.listdir(dir):
-    #     img_path = os.path.join(dir, filename)
-    #     img_paths.append(img_path)
-
+        classify_image_and_others(img_path, filename, average_saturation)
     
-    img_path = 'data_collection_img/image.jpg'
-    
-    edges_detected = detect_edges(img_path)
-
-    contours_found = find_contours(edges_detected)
-
 
 if __name__ == "__main__":
     main()
