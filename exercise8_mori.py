fb46fd85b98325506feb8fbb198be0da61ee8805
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-09-01 17:57:42 +0900

    feat: classify images and text/diagram based on saturation

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -3,22 +3,6 @@ import numpy as np
 import os
 
 
-def detect_edges(img_path):
-    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
-
-    blur = cv.GaussianBlur(img, (3, 3), 3)
-
-    edge_detection = cv.Canny(blur, threshold1=100, threshold2=200, L2gradient=True)
-
-    return edge_detection
-
-
-def find_contours(edge_detected):
-    contours, hierarchies = cv.findContours(edge_detected, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
-    return contours
-
-
 def calc_average_saturation(img_path):
     image = cv.imread(img_path)
 
@@ -34,21 +18,17 @@ def calc_average_saturation(img_path):
 def classify_image_and_others(img_path, filename, saturation):
     img = cv.imread(img_path)
 
-    if saturation > 5:
+    if saturation > 3:
         cv.imwrite('result/image/' + filename, img)
     else:
-        pass
+        cv.imwrite('result/text_and_diagram/' + filename, img)
+
 
 
 def main():
-    dir = 'test'
-    for filename in os.listdir(dir):
-        img_path = os.path.join(dir, filename)
-            
-        average_saturation = calc_average_saturation(img_path)
 
-        classify_image_and_others(img_path, filename, average_saturation)
-    
+    img_path = 'test/test-046.jpg'
+
 
 if __name__ == "__main__":
     main()
