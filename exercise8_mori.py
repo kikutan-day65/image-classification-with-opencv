aef6ab0280b99d24b6566fdaeab9f9f8c2d11c7c
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-09-13 17:00:02 +0900

    feat: harris corner detection

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -15,6 +15,23 @@ def calc_average_saturation(img_path):
     return average_saturation
 
 
+def harris_corners(img_path):
+    img = cv.imread(img_path)
+    img = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    gray = np.float32(gray)
+    dst = cv.cornerHarris(gray, 2, 3, 0.04)
+
+    dst = cv.dilate(dst, None)
+
+    img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
+    cv.imshow('dst', img)
+    if cv.waitKey(0) & 0xff == 27:
+        cv.destroyAllWindows()
+
+
 def classify_image_and_others(img_path, filename, saturation):
     img = cv.imread(img_path)
 
