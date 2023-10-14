9102b310d2f9af63fc72181b9c1d11e3838920ab
haruto <haruto@Ubuntu.myguest.virtualbox.org>
2023-10-14 18:16:52 +0900

    furnction to extract pages from pdf

--- exercise8_mori.py
+++ exercise8_mori.py
@@ -1,10 +1,25 @@
 import cv2 as cv
 import numpy as np
 import os
+from pdf2image import convert_from_path
+
+
+def extract_pdf(pdf_path):
+    pdf = convert_from_path(pdf_path)
+
+    if pdf is None:
+        print('you might add wrong path')
+
+    for i in range(len(pdf)):
+        pdf[i].save('page_' + str(i + 1) + '.jpg', 'JPEG')
+    print('Extracted successfully!')
 
 
 def main():
-    pass
+
+    # add path to pdf to be extracted
+    # pdf_path = ''
+    # extract_pdf(pdf_path)
 
 
 if __name__ == "__main__":
